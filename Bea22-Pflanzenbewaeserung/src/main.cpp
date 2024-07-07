f06c8760c1cbd1bf4b39392d7cda854fb1834194
ttonin33 <ttonin33@gmail.com>
2024-07-07 18:03:34 +0200

    OTA secure

--- Bea22-Pflanzenbewaeserung/src/main.cpp
+++ Bea22-Pflanzenbewaeserung/src/main.cpp
@@ -30,7 +30,7 @@ void setup()
 
     // Password can be set with it's md5 value as well
     // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
-    // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
+    ArduinoOTA.setPasswordHash("4b24450591bbca92d6c7f4e650ee309a");
 
     ArduinoOTA
         .onStart([]()
